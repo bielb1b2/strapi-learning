4724b49aa3737aaa69d9c7cf006cab87d9a3e45f
Gabriel Souza <gabrielvt.souza@hotmail.com>
2021-06-08 11:47:52 -0300

    first commit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '40a0a645581dc3fed4a69410b22e6db9'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '60f75684-c817-43ba-8ccb-6f3d0b69d549'
+};
